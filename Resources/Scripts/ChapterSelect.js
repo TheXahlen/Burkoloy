2713d4518beed5e357cede77b6b4db6f6b9c8ac9
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-03-04 15:12:06 -0600

    Update ChapterSelect.js

--- Resources/Scripts/ChapterSelect.js
+++ Resources/Scripts/ChapterSelect.js
@@ -5,10 +5,15 @@ function ChapterSelect() {
   var url = "https://xplosivex.github.io/Burkoloy/Chapters/Chapter" + currentVal + ".html";
 var fruits = ["1", "2", "3", "4", "5", "6", "7", "8", "9", 
 "10", "11", "12", "13", "14", "15", "16", "17",  ];
-if (n = fruits.includes(currentVal)) {
+var numbers =  a[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", 
+"ten", "eleven", "tweleve", "thirteen", "fourteen", "fiftheen", "sixteen", "seventeen"  }, M = currentVal;
+if (n = numbers.includes(currentVal)) { 
+
+}  
+else if (n = fruits.includes(currentVal)) {
 location.href= url;
 } else {
 document.getElementById("demo").innerHTML =
-    "<p> Not a chapter </p>" 
+    numbers  
 }
 }
