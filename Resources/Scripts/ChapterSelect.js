4808e658fbd7f532e40a3092df37b9d82a4fc3bd
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-03-04 12:13:35 -0600

    Create ChapterSelect.js

---
+++ Resources/Scripts/ChapterSelect.js
@@ -0,0 +1,14 @@
+function ChapterSelect() {
+
+  var x = document.getElementById("myPsw");
+  var currentVal = x.value; 
+  var url = "https://xplosivex.github.io/Burkoloy/Chapters/Chapter" + currentVal + ".html";
+var fruits = ["1", "2", "3", "4", "5", "6", "7", "8", "9", 
+"10", "11", "12", "13", "14", "15", "16", "17" ];
+if (n = fruits.includes(currentVal)) {
+location.href= url;
+} else {
+document.getElementById("demo").innerHTML =
+    "<p> Not a chapter </p>" 
+}
+}
