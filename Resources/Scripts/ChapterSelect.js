734929920c35d83fa63b27d5aa981abe43fb0fc0
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-03-04 15:24:46 -0600

    Update ChapterSelect.js

--- Resources/Scripts/ChapterSelect.js
+++ Resources/Scripts/ChapterSelect.js
@@ -3,17 +3,20 @@ function ChapterSelect() {
   var x = document.getElementById("myPsw");
   var currentVal = x.value; 
   var url = "https://xplosivex.github.io/Burkoloy/Chapters/Chapter" + currentVal + ".html";
+   var url2 = "https://xplosivex.github.io/Burkoloy/Chapters/Chapter" + index + ".html";
+   var index = imageList.indexOf(currentVal);
 var fruits = ["1", "2", "3", "4", "5", "6", "7", "8", "9", 
 "10", "11", "12", "13", "14", "15", "16", "17",  ];
-var numbers =  a[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", 
-"ten", "eleven", "tweleve", "thirteen", "fourteen", "fiftheen", "sixteen", "seventeen"  }, M = currentVal;
+var imageList = ["ddsfdsfds", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", 
+"ten", "eleven", "tweleve", "thirteen", "fourteen", "fiftheen", "sixteen", "seventeen" ]; 
 if (n = numbers.includes(currentVal)) { 
 
+location.href= url2;
 }  
 else if (n = fruits.includes(currentVal)) {
 location.href= url;
 } else {
 document.getElementById("demo").innerHTML =
-    numbers  
+    K 
 }
 }
