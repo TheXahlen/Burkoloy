7ebb0b5f3c2c7be0a3e6ebb1d9efd709c484a6c4
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-03-06 08:33:04 -0600

    Update ChapterSelect.js

--- Resources/Scripts/ChapterSelect.js
+++ Resources/Scripts/ChapterSelect.js
@@ -2,27 +2,32 @@ function ChapterSelect() {
 
   var x = document.getElementById("myPsw");
   var currentVal = x.value; 
-
-var fruits = ["1", "2", "3", "4", "5", "6", "7", "8", "9", 
+var zeros = [ "0" , "Zero" , "zero"];
+var arraynumbers = [ "1", "2", "3", "4", "5", "6", "7", "8", "9", 
 "10", "11", "12", "13", "14", "15", "16", "17",  ];
-var beasts = ["ddsfdsfds", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", 
+var arraylower = ["sajfdijafidsjifsd", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", 
 "ten", "eleven", "tweleve", "thirteen", "fourteen", "fiftheen", "sixteen", "seventeen"];
-var CapitalBeasts = ["ddsfdsfds", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", 
+var arraycapital = ["asfjsajkfodasjkfoskf", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", 
 "Ten", "Eleven", "Tweleve", "Thirteen", "Fourteen", "Fiftheen", "Sixteen", "Seventeen"];
-var Collor = (beasts.indexOf(currentVal)); 
-var Collor2 = (CapitalBeasts.indexOf(currentVal)); 
-    var url = "https://xplosivex.github.io/Burkoloy/Chapters/Chapter" + currentVal + ".html";
+var Collor = (arraylower.indexOf(currentVal)); 
+var Collor2 = (arraycapital.indexOf(currentVal)); 
+    var url1 = "https://xplosivex.github.io/Burkoloy/Chapters/Chapter" + currentVal + ".html";
    var url2 = "https://xplosivex.github.io/Burkoloy/Chapters/Chapter" + Collor + ".html";
-    var url3 = "https://xplosivex.github.io/Burkoloy/Chapters/Chapter" + Collor2 + ".html";
-if (n = beasts.includes(currentVal)) { 
+   var url3 = "https://xplosivex.github.io/Burkoloy/Chapters/Chapter" + Collor2 + ".html";
+  var url4 = "https://xplosivex.github.io/Burkoloy/";
+  
+ if (n = currentVal.includes(zeros)) {
+ location.href= url1;
+ }
+else if (n = arraylower.includes(currentVal)) { 
 
 location.href= url2;
 }  
-else if (n = CapitalBeasts.includes(currentVal)) {
+else if (n = arraycapital.includes(currentVal)) {
 location.href= url3;
 }
-else if (n = fruits.includes(currentVal)) {
-location.href= url;
+else if (n = arraynumbers.includes(currentVal)) {
+location.href= url1;
 } else {
 document.getElementById("demo").innerHTML =
     "Not A  Valid Chapter"
