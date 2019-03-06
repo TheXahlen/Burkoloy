fa85dc2a92d513e30aa9109baa7b1c70cb8a2492
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-03-06 09:07:56 -0600

    Update Dynamic.js

--- Resources/Scripts/Dynamic.js
+++ Resources/Scripts/Dynamic.js
@@ -1,32 +1,4 @@
-// Parse the URL parameter
-	function getParameterByName(name, url) {
-	    if (!url) url = window.location.href;
-	    name = name.replace(/[\[\]]/g, "\\$&");
-	    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-	        results = regex.exec(url);
-	    if (!results) return null;
-	    if (!results[2]) return '';
-	    return decodeURIComponent(results[2].replace(/\+/g, " "));
-	}
-	// Give the parameter a variable name
-	var dynamicContent = getParameterByName('dc');
- 
-	 $(document).ready(function() {
- 
-		// Check if the URL parameter is apples
-		if (dynamicContent == 'apples') {
-			$('#apples').show();
-		} 
-		// Check if the URL parameter is oranges
-		else if (dynamicContent == 'oranges') {
-			$('#oranges').show();
-		} 
-		// Check if the URL parameter is bananas
-		else if (dynamicContent == 'bananas') {
-			$('#bananas').show();
-		} 
-		// Check if the URL parmeter is empty or not defined, display default content
-		else {
-			$('#default-content').show();
-		}
-	});
+var url = window.location.href;
+var numb = url.match(/\d/g);
+numb = numb.join("");
+document.getElementById(numb).className = "active";
