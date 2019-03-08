f277ff3627f88a20789dca115c8275f6860c0d53
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-03-08 09:22:30 -0600

    Update TextSizer.js

--- Resources/Scripts/TextSizer.js
+++ Resources/Scripts/TextSizer.js
@@ -3,15 +3,27 @@ function Enlarge() {
 
 if (x.id == "Normal") { 
  document.getElementById("Sizeable").style.fontSize = "25px";
+  document.getElementById("Sizeable2").style.fontSize = "25px";
+  document.getElementById("Sizeable3").style.fontSize = "25px";
+  document.getElementById("Sizeable4").style.fontSize = "25px";
  document.getElementById("Normal").id = "Big1";
 } else if (x.id == "Big1")  {
 document.getElementById("Sizeable").style.fontSize = "30px";
+  document.getElementById("Sizeable2").style.fontSize = "30px";
+  document.getElementById("Sizeable3").style.fontSize = "30px";
+  document.getElementById("Sizeable4").style.fontSize = "30px";
 document.getElementById("Big1").id = "Big2";
 } else if (x.id == "Small2") {
 document.getElementById("Sizeable").style.fontSize = "12px";
+  document.getElementById("Sizeable2").style.fontSize = "12px";
+  document.getElementById("Sizeable3").style.fontSize = "12px";
+  document.getElementById("Sizeable4").style.fontSize = "12px";
 document.getElementById("Small2").id = "Small1"; 
 } else if  (x.id == "Small1")  {
 document.getElementById("Sizeable").style.fontSize = "16px";
+  document.getElementById("Sizeable2").style.fontSize = "16px";
+  document.getElementById("Sizeable3").style.fontSize = "16px";
+  document.getElementById("Sizeable4").style.fontSize = "16px";
 document.getElementById("Small1").id = "Normal"; 
 }
 }
@@ -20,18 +32,33 @@ function Shrink() {
 
 if (x.id == "Normal") { 
  document.getElementById("Sizeable").style.fontSize = "12px";
+  document.getElementById("Sizeable2").style.fontSize = "12px";
+   document.getElementById("Sizeable3").style.fontSize = "12px";
+   document.getElementById("Sizeable4").style.fontSize = "12px";
  document.getElementById("Normal").id = "Small1";
 } else if (x.id == "Small1")  {
 document.getElementById("Sizeable").style.fontSize = "10px";
+  document.getElementById("Sizeable2").style.fontSize = "10px";
+  document.getElementById("Sizeable3").style.fontSize = "10px";
+  document.getElementById("Sizeable4").style.fontSize = "10px";
 document.getElementById("Small1").id = "Small2";
 } else if (x.id == "Big2") {
 document.getElementById("Sizeable").style.fontSize = "25px";
+  document.getElementById("Sizeable2").style.fontSize = "25px";
+  document.getElementById("Sizeable3").style.fontSize = "25px";
+  document.getElementById("Sizeable4").style.fontSize = "25px";
 document.getElementById("Big2").id = "Big1"; 
 } else if  (x.id == "Big1")  {
 document.getElementById("Sizeable").style.fontSize = "16px";
+  document.getElementById("Sizeable2").style.fontSize = "16px";
+  document.getElementById("Sizeable3").style.fontSize = "16px";
+  document.getElementById("Sizeable4").style.fontSize = "16px";
 document.getElementById("Big1").id = "Normal"; 
 }
 }
 function  Reset(){ 
 document.getElementById("Sizeable").style.fontSize = "16px";
+  document.getElementById("Sizeable2").style.fontSize = "16px";
+  document.getElementById("Sizeable3").style.fontSize = "16px";
+  document.getElementById("Sizeable4").style.fontSize = "16px";
 }
