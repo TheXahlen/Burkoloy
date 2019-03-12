6ad3dbebf065e227a5a2e0890443a3721745c4ad
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-03-12 14:15:52 -0500

    Update Greek.js

--- Resources/Scripts/Greek.js
+++ Resources/Scripts/Greek.js
@@ -1,19 +1,16 @@
 
-function draw() {
-  
-var time = Math.round(World.frameCount/10);
 
-
-
-if (time == 1){
-  var screen1 = createSprite(200,0, 100, 100)
-screen1.setAnimation("Greek")
-
+function cycle() {
+    background("black");
+function draw1(){
+  var screen1 = createSprite(200,0, 100, 100);
+screen1.setAnimation("Greek");
 screen1.x = 200;
 screen1.y = 0;
 drawSprites();
 }
-if (time == 2){
+setTimeout(draw1, 650);
+function draw2(){
 var fullwhite1 = createSprite(200,200,100,100);
 fullwhite1.setAnimation("animation_1");
 fullwhite1.scale = 100;
@@ -22,77 +19,78 @@ drawSprites();
 
 
 }
-if (time == 3){
-var screen2 = createSprite(200,0, 100, 100)
+setTimeout(draw2, 1000);
+ function draw3(){
+var screen2 = createSprite(200,0, 100, 100);
 screen2.setAnimation("Greek");
 screen2.x = 200;
 screen2.y = 100;
 drawSprites();  
 }
-if (time == 4){
+setTimeout(draw3, 1500);
+function draw4(){
 var fullwhite2 = createSprite(200,200,100,100);
 fullwhite2.setAnimation("animation_1");
 fullwhite2.scale = 100;
 drawSprites();
-
-
-
 }
-if (time == 5){
-var screen3 = createSprite(200,0, 100, 100)
+setTimeout(draw4, 2000);
+function draw5(){
+var screen3 = createSprite(200,0, 100, 100);
 screen3.setAnimation("Greek");
 screen3.x = 200;
 screen3.y = 200;
 drawSprites();  
 }
-if (time == 6){
+setTimeout(draw5, 2500);
+function draw6(){
 var fullwhite3 = createSprite(200,200,100,100);
 fullwhite3.setAnimation("animation_1");
 fullwhite3.scale = 100;
 drawSprites();
 
-
-
 }
-if (time == 7){
-var screen4 = createSprite(200,0, 100, 100)
+setTimeout(draw6, 3000);
+function draw7() {
+var screen4 = createSprite(200,0, 100, 100);
 screen4.setAnimation("Greek");
 screen4.x = 200;
 screen4.y = 300;
 drawSprites();  
 }
-if (time == 8){
+setTimeout(draw7, 3500);
+function draw8(){
 var fullwhite4 = createSprite(200,200,100,100);
 fullwhite4.setAnimation("animation_1");
 fullwhite4.scale = 100;
 drawSprites();
-
-
-
 }
-if (time == 9){
-var screen5 = createSprite(200,0, 100, 100)
+setTimeout(draw8, 4000);
+function draw9(){
+var screen5 = createSprite(200,0, 100, 100);
 screen5.setAnimation("Greek");
 screen5.x = 200;
 screen5.y = 400;
 drawSprites();  
 }
-if (time == 10){
-var screen6 = createSprite(200,0, 100, 100)
+setTimeout(draw9, 4500);
+function draw10(){
+var screen6 = createSprite(200,0, 100, 100);
 screen6.setAnimation("Boom");
 screen6.x = 200;
 screen6.y = 200;
 screen6.scale = 3;
 drawSprites();  
 }
-if (time == 11){
-
+setTimeout(draw10, 5000);
+function draw11(){
+var fullwhite5 = createSprite(200,200,100,100);
+fullwhite5.setAnimation("animation_1");
+fullwhite5.scale = 100;
+drawSprites();
 }
+setTimeout(draw11, 5200);
+   setTimeout(cycle, 5200);
 }
 
-
-
-
-
-
-
+cycle();
