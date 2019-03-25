1903024aa73e94ce2fce6a55a5e9f31ccabef3b0
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-03-25 09:37:26 -0500

    Update AntiCopy.js

--- Resources/Scripts/AntiCopy.js
+++ Resources/Scripts/AntiCopy.js
@@ -1,34 +1,17 @@
 function AntiCopy(){
-
-}
 var z = 0;
 document.addEventListener('copy', function(e) {
-  AntiCopy();
-  var x = z++;
-  var PosNames = [ ];
-  console.log(x)
-  if (x == 0) {
-  var Copy = "Please Don't Copy Or Reproduce This Content";  
-  }
-  if (x == 1) {
-  var Copy = "Please Do Not Copy This Material";
-  }
-if (x == 2) {
-  var Copy = "Don't Make Me Ask Again.";
-  }
-  if (x == 3) {
-  var Copy = "Joe Will Smite You";
-  }
-  if (x == 3) {
-  var Copy = "STOP FUCKING COPYING THIS";
-  }
-  if (x == 4){
-  var Copy = "You're Not Gonna Copy It Stop Trying";
-  }
-  if(x > 4) {
-  var z = 0; 
-  }
-  
+var x = z++;
+console.log(x)
+if (x == 0) {var Copy = "Please Don't Copy Or Reproduce This Content";}
+if (x == 1) {var Copy = "Please Do Not Copy This Material";}
+if (x == 2) {var Copy = "Don't Make Me Ask Again.";}
+if (x == 3) {var Copy = "Joe Will Smite You";}
+if (x == 3) {var Copy = "STOP FUCKING COPYING THIS";}
+if (x == 4) {var Copy = "You're Not Gonna Copy It Stop Trying";}
+if (x == 5) {var Copy = "stop actually like for real. eeee"; z = 0;}
+  console.log(Copy)
   e.clipboardData.setData('text/plain', Copy);
   e.preventDefault();
 });
+}
